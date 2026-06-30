91e737566d7203f0e0262bdb7b091770fcf80032
AnujBhushanTiw2685 <anujbhushantiwari2022@vitbhopal.ac.in>
2026-06-30 09:31:49 +0000

    first commit

---
+++ entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e # exit immediately if any command fails
+
+# Entrypoint for test-scout Github Action
+# 1. Count total Python files (*.py)
+# 2. Count test files based on patten (Default : test_*.py)
+# 3. If no test files are found, do something (warn or fail)
+# 4. Otherwise, exit gracefully
+
+
+echo "🕑 test-scout is running..."
+
+echo 
+exit 0
+
+
